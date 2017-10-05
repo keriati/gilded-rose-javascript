caa774127f3be350849fdff21d8edc2841a2db22
Marek Tucani <marektucani@Mareks-MacBook-Pro.local>
2017-10-05 15:46:43 +0200

    Refactored code of shop method updateQuality, extracted functions and constants

--- src/__tests__/gilded_rose_spec.js
+++ src/__tests__/gilded_rose_spec.js
@@ -1,13 +1,13 @@
 import {
   Shop,
   Item,
-  NAME_AGED_BRIE,
-  NAME_SULFURAS,
-  NAME_BACKSTAGE_PASSES,
+  AGED_BRIE,
+  SULFURAS,
+  BACKSTAGE_PASSES,
   CONJURED_PREFIX,
 } from '../gilded_rose';
 
-const NAME_NORMAL = 'a';
+const NORMAL_ITEM = 'a';
 
 const SELL_IN_NORMAL = 10;
 const SELL_IN_ZERO = 0;
@@ -28,9 +28,9 @@ function createShopWithItem(itemName, sellIn, quality) {
 
 describe('Item', () => {
   it('has properties correctly defined', () => {
-    const i = new Item(NAME_NORMAL, SELL_IN_NORMAL, QUALITY_NORMAL);
+    const i = new Item(NORMAL_ITEM, SELL_IN_NORMAL, QUALITY_NORMAL);
 
-    expect(i.name).toEqual(NAME_NORMAL);
+    expect(i.name).toEqual(NORMAL_ITEM);
     expect(i.quality).toEqual(QUALITY_NORMAL);
     expect(i.sellIn).toEqual(SELL_IN_NORMAL);
   });
@@ -44,94 +44,94 @@ describe('Shop', () => {
     expect(shop.items.length).toBe(0);
   });
   it('has non empty items when non empty array is passed', () => {
-    const shop = createShopWithItem(NAME_NORMAL, SELL_IN_NORMAL, QUALITY_NORMAL);
+    const shop = createShopWithItem(NORMAL_ITEM, SELL_IN_NORMAL, QUALITY_NORMAL);
 
     expect(shop.items.length).toBe(1);
   });
 });
 
-describe('Gilded Rose Rules: ', () => {
+describe('Rules ', () => {
   it('lower quality and sellIn every day by 1', () => {
-    const shop = createShopWithItem(NAME_NORMAL, SELL_IN_NORMAL, QUALITY_NORMAL);
+    const shop = createShopWithItem(NORMAL_ITEM, SELL_IN_NORMAL, QUALITY_NORMAL);
 
-    const itemsDay1 = shop.endOfDay();
+    const itemsDay1 = shop.updateQuality();
 
     expect(itemsDay1[0].sellIn).toEqual(SELL_IN_NORMAL - 1);
     expect(itemsDay1[0].quality).toEqual(SELL_IN_NORMAL - 1);
   });
   it('degrade quality by 2 every day when sellIn is 0', () => {
-    const shop = createShopWithItem(NAME_NORMAL, SELL_IN_ZERO, QUALITY_NORMAL);
+    const shop = createShopWithItem(NORMAL_ITEM, SELL_IN_ZERO, QUALITY_NORMAL);
 
-    const itemsDay1 = shop.endOfDay();
+    const itemsDay1 = shop.updateQuality();
 
     expect(itemsDay1[0].quality).toEqual(QUALITY_NORMAL - 2);
   });
 
   describe('Quality', () => {
     it('can not be <0', () => {
-      const shop = createShopWithItem(NAME_NORMAL, SELL_IN_NORMAL, QUALITY_ZERO);
+      const shop = createShopWithItem(NORMAL_ITEM, SELL_IN_NORMAL, QUALITY_ZERO);
 
-      const itemsDay1 = shop.endOfDay();
+      const itemsDay1 = shop.updateQuality();
 
       expect(itemsDay1[0].quality).toEqual(QUALITY_MIN);
     });
     it('can not be >50', () => {
       // Aged Brie increases quality over time
-      const shop = createShopWithItem(NAME_AGED_BRIE, SELL_IN_NORMAL, QUALITY_MAX);
+      const shop = createShopWithItem(AGED_BRIE, SELL_IN_NORMAL, QUALITY_MAX);
 
-      const itemsDay1 = shop.endOfDay();
+      const itemsDay1 = shop.updateQuality();
 
       expect(itemsDay1[0].quality).toEqual(QUALITY_MAX);
     });
   });
 
-  describe(`Special item: ${NAME_AGED_BRIE}`, () => {
+  describe(`Special item: ${AGED_BRIE}`, () => {
     it('increases quality by 2 every day', () => {
-      const shop = createShopWithItem(NAME_AGED_BRIE, SELL_IN_NORMAL, QUALITY_NORMAL);
+      const shop = createShopWithItem(AGED_BRIE, SELL_IN_NORMAL, QUALITY_NORMAL);
 
-      const itemsDay1 = shop.endOfDay();
+      const itemsDay1 = shop.updateQuality();
 
       expect(itemsDay1[0].quality).toEqual(QUALITY_NORMAL + 2);
     });
   });
 
-  describe(`Special item: ${NAME_SULFURAS}`, () => {
+  describe(`Special item: ${SULFURAS}`, () => {
     it('does not decrease quality or sellIn over time', () => {
-      const shop = createShopWithItem(NAME_SULFURAS, SELL_IN_NORMAL, QUALITY_LEGENDARY);
+      const shop = createShopWithItem(SULFURAS, SELL_IN_NORMAL, QUALITY_LEGENDARY);
 
-      const itemsDay1 = shop.endOfDay();
+      const itemsDay1 = shop.updateQuality();
 
       expect(itemsDay1[0].quality).toEqual(QUALITY_LEGENDARY);
       expect(itemsDay1[0].sellIn).toEqual(SELL_IN_NORMAL);
     });
   });
 
-  describe(`Special item: ${NAME_BACKSTAGE_PASSES}`, () => {
+  describe(`Special item: ${BACKSTAGE_PASSES}`, () => {
     it('increases quality by 1 every day when sellIn in (Infinity, 10)  ', () => {
-      const shop = createShopWithItem(NAME_BACKSTAGE_PASSES, SELL_IN_INTERVAL_INFINITY_TO_10, QUALITY_NORMAL);
+      const shop = createShopWithItem(BACKSTAGE_PASSES, SELL_IN_INTERVAL_INFINITY_TO_10, QUALITY_NORMAL);
 
-      const itemsDay1 = shop.endOfDay();
+      const itemsDay1 = shop.updateQuality();
 
       expect(itemsDay1[0].quality).toEqual(QUALITY_NORMAL + 1);
     });
     it('increases quality by 2 every day when sellIn in <10, 5)', () => {
-      const shop = createShopWithItem(NAME_BACKSTAGE_PASSES, SELL_IN_INTERVAL_10_TO_5, QUALITY_NORMAL);
+      const shop = createShopWithItem(BACKSTAGE_PASSES, SELL_IN_INTERVAL_10_TO_5, QUALITY_NORMAL);
 
-      const itemsDay1 = shop.endOfDay();
+      const itemsDay1 = shop.updateQuality();
 
       expect(itemsDay1[0].quality).toEqual(QUALITY_NORMAL + 2);
     });
     it('increases quality by 3 every day when sellIn in <5, 0)', () => {
-      const shop = createShopWithItem(NAME_BACKSTAGE_PASSES, SELL_IN_INTERVAL_5_TO_0, QUALITY_NORMAL);
+      const shop = createShopWithItem(BACKSTAGE_PASSES, SELL_IN_INTERVAL_5_TO_0, QUALITY_NORMAL);
 
-      const itemsDay1 = shop.endOfDay();
+      const itemsDay1 = shop.updateQuality();
 
       expect(itemsDay1[0].quality).toEqual(QUALITY_NORMAL + 3);
     });
     it('drops quality to 0 when sellIn is 0', () => {
-      const shop = createShopWithItem(NAME_BACKSTAGE_PASSES, SELL_IN_ZERO, QUALITY_NORMAL);
+      const shop = createShopWithItem(BACKSTAGE_PASSES, SELL_IN_ZERO, QUALITY_NORMAL);
 
-      const itemsDay1 = shop.endOfDay();
+      const itemsDay1 = shop.updateQuality();
 
       expect(itemsDay1[0].quality).toEqual(QUALITY_ZERO);
     });
@@ -140,18 +140,18 @@ describe('Gilded Rose Rules: ', () => {
   describe('Special item: "Conjured ...', () => {
     it('lowers quality by 2 and sellIn by 1 every day', () => {
       // twice that fast as other normal items
-      const shop = createShopWithItem(`${CONJURED_PREFIX} ${NAME_NORMAL}`, SELL_IN_NORMAL, QUALITY_NORMAL);
+      const shop = createShopWithItem(`${CONJURED_PREFIX} ${NORMAL_ITEM}`, SELL_IN_NORMAL, QUALITY_NORMAL);
 
-      const itemsDay1 = shop.endOfDay();
+      const itemsDay1 = shop.updateQuality();
 
       expect(itemsDay1[0].sellIn).toEqual(SELL_IN_NORMAL - 1);
       expect(itemsDay1[0].quality).toEqual(QUALITY_NORMAL - 2);
     });
-    it('degrade quality by 4 every day when sellIn is 0', () => {
+    it('degrades quality by 4 every day when sellIn is 0', () => {
       // twice that fast as other normal items
-      const shop = createShopWithItem(`${CONJURED_PREFIX} ${NAME_NORMAL}`, SELL_IN_ZERO, QUALITY_NORMAL);
+      const shop = createShopWithItem(`${CONJURED_PREFIX} ${NORMAL_ITEM}`, SELL_IN_ZERO, QUALITY_NORMAL);
 
-      const itemsDay1 = shop.endOfDay();
+      const itemsDay1 = shop.updateQuality();
 
       expect(itemsDay1[0].sellIn).toEqual(SELL_IN_ZERO);
       expect(itemsDay1[0].quality).toEqual(QUALITY_NORMAL - 4);
--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -1,6 +1,21 @@
-export const NAME_AGED_BRIE = 'Aged Brie';
-export const NAME_SULFURAS = 'Sulfuras, Hand of Ragnaros';
-export const NAME_BACKSTAGE_PASSES = 'Backstage passes to a TAFKAL80ETC concert';
+export const AGED_BRIE = 'Aged Brie';
+export const SULFURAS = 'Sulfuras, Hand of Ragnaros';
+export const BACKSTAGE_PASSES = 'Backstage passes to a TAFKAL80ETC concert';
+
+const TICKET_QUALITY_DOUBLE_DAYS_LIMIT = 10;
+const TICKET_QUALITY_TRIPLE_DAYS_LIMIT = 5;
+
+export const LEGENDARY_ITEMS = [
+  SULFURAS,
+];
+
+export const CHEESE_ITEMS = [
+  AGED_BRIE,
+];
+
+export const TICKET_ITEMS = [
+  BACKSTAGE_PASSES,
+];
 
 export const CONJURED_PREFIX = 'Conjured';
 
@@ -12,39 +27,102 @@ export class Item {
   }
 }
 
-const isConjured = function(item) {
+const isConjured = function (item) {
   return item.name.startsWith(CONJURED_PREFIX);
 };
 
-const updateSellIn = function(item) {
-  if (item.name === NAME_SULFURAS) return;
-  item.sellIn = item.sellIn - 1;
+const isPastSellInDate = function (item) {
+  return item.sellIn <= 0;
+};
+
+const isLegendary = function (item) {
+  return LEGENDARY_ITEMS.includes(item.name);
+};
+
+const isCheese = function (item) {
+  return CHEESE_ITEMS.includes(item.name);
+};
+
+const isTicket = function (item) {
+  return TICKET_ITEMS.includes(item.name);
+};
+
+const isNormal = function (item) {
+  return !isLegendary(item) && !isCheese(item) && !isTicket(item);
+};
+
+const decreaseSellIn = function (item, amount = 1) {
+  item.sellIn -= amount;
+};
+
+const decreaseQuality = function (item, amount = 1) {
+  item.quality -= amount;
+};
+
+const increaseQuality = function (item, amount = 1) {
+  item.quality += amount;
+};
+
+const resetQueality = function (item) {
+  item.quality = 0;
+};
+
+const applyQualityLimits = function (item) {
+  item.quality = Math.min(item.quality, 50);
+  item.quality = Math.max(item.quality, 0);
+};
+
+const applySellInLimits = function (item) {
   item.sellIn = Math.max(item.sellIn, 0);
 };
 
-const updateQuality = function (item) {
-  if (item.name === NAME_SULFURAS) return;
+const getTicketQualityIncreaseAmount = function (item) {
+  const daysLeft = item.sellIn;
+  let qualityRate = 1;
 
-  const rate = isConjured(item) ? 2 : 1;
-
-  if (item.name === NAME_AGED_BRIE) {
-    item.quality = item.quality + 2;
-  } else if (item.name === NAME_BACKSTAGE_PASSES) {
-    if (item.sellIn === 0) {
-      item.quality = 0;
-    } else if (item.sellIn <= 5) {
-      item.quality = item.quality + 3;
-    } else if (item.sellIn <= 10) {
-      item.quality = item.quality + 2;
-    } else {
-      item.quality = item.quality + 1;
-    }
-  } else {
-    const qualityDecrease = item.sellIn === 0 ? 2 : 1;
-    item.quality = item.quality - qualityDecrease * rate;
+  if (daysLeft <= TICKET_QUALITY_TRIPLE_DAYS_LIMIT) {
+    qualityRate += 1;
   }
-  item.quality = Math.min(item.quality, 50);
-  item.quality = Math.max(item.quality, 0);
+  if (daysLeft <= TICKET_QUALITY_DOUBLE_DAYS_LIMIT) {
+    qualityRate += 1;
+  }
+  return qualityRate;
+};
+
+const updateSellIn = function (item) {
+  if (isLegendary(item)) {
+    return;
+  }
+  decreaseSellIn(item);
+  applySellInLimits(item);
+};
+
+const updateQuality = function (item) {
+  if (isLegendary(item)) {
+    return;
+  }
+
+  if (isCheese(item)) {
+    increaseQuality(item, 2);
+    applyQualityLimits(item);
+  }
+
+  if (isTicket(item)) {
+    if (isPastSellInDate(item)) {
+      resetQueality(item);
+    } else {
+      const qualityIncrease = getTicketQualityIncreaseAmount(item);
+      increaseQuality(item, qualityIncrease);
+    }
+    applyQualityLimits(item);
+  }
+
+  if (isNormal(item)) {
+    const qualityDecreaseRate = isConjured(item) ? 2 : 1;
+    const qualityDecrease = isPastSellInDate(item) ? 2 : 1;
+    decreaseQuality(item, qualityDecrease * qualityDecreaseRate);
+    applyQualityLimits(item);
+  }
 };
 
 export class Shop {
@@ -52,7 +130,7 @@ export class Shop {
     this.items = items;
   }
 
-  endOfDay() {
+  updateQuality() {
     this.items.forEach((item) => {
       updateSellIn(item);
       updateQuality(item);
