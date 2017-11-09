c1a1cf4b0444d97d6a288b65ff8b2fd617d67248
Marek Tobias <tobi@Mareks-MacBook-Pro.local>
2017-11-09 15:04:36 +0100

    First attempt

--- src/__tests__/gilded_rose_spec.js
+++ src/__tests__/gilded_rose_spec.js
@@ -1,11 +1,218 @@
-import { Shop, Item } from '../gilded_rose';
+import {
+  TICKET_NAME,
+  CHEESE_NAME,
+  SULFURAS,
+  CONJURED,
 
-describe("Gilded Rose", function () {
+  Shop,
+  Item,
+} from '../gilded_rose';
 
-    it("should foo", function () {
-        const gildedRose = new Shop([new Item("foo", 0, 0)]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).toEqual("fixme");
-    });
 
+const QUALITY_GT_0 = 1
+const QUALITY_0 = 0
+const QUALITY_LT_0 = -1
+
+const QUALITY_GT_50 = 51
+const QUALITY_50 = 50
+
+const QUALITY_1 = 1
+const QUALITY_2 = 2
+const QUALITY_3 = 3
+const QUALITY_10 = 10
+
+
+const SELL_IN_1 = 1
+const SELL_IN_LT_ZERO = -1
+const SELL_IN_ZERO = 0
+
+// cheese
+const SELL_IN_GT_TEN = 11
+const SELL_IN_BETWEEN_11_AND_6 = 6
+const SELL_IN_LT_6 = 5
+
+const updateQuality = (item) => {
+  const shop = new Shop([new Item(item.name, item.sellIn, item.quality)])
+  return shop.updateQuality()[0]
+}
+
+describe('Gilded Rose', function () {
+  describe('quality mechanism', () => {
+    describe('common behvaior', () => {
+
+      it('decrease every item quality by 1 if quality > 0', () => {
+        const item = { quality: QUALITY_GT_0 }
+
+        const actual = updateQuality(item).quality
+
+        expect(actual).toBe(QUALITY_0)
+      })
+
+      it('bounds minimum quality to 0', () => {
+        const item = { quality: QUALITY_LT_0 }
+
+        const actual = updateQuality(item).quality
+
+        expect(actual).toBe(QUALITY_0)
+      })
+
+      it('bounds quality maximum to 50', () => {
+        const item = { quality: QUALITY_GT_50 }
+
+        const actual = updateQuality(item).quality
+
+        expect(actual).toBe(QUALITY_50)
+      })
+
+      it(`set quality to 0 if sellIn < 0`, () => {
+        const item = { 
+          quality: QUALITY_GT_0,
+          sellIn: SELL_IN_LT_ZERO,
+        }
+
+        const actual = updateQuality(item).quality
+
+        expect(actual).toBe(QUALITY_0)
+      })
+      
+      it(`decrease quality by 2 if sellIn < 0`, () => {
+        const item = { 
+          quality: QUALITY_2,
+          sellIn: SELL_IN_LT_ZERO,
+        }
+
+        const actual = updateQuality(item).quality
+
+        expect(actual).toBe(QUALITY_0)
+      })
+    })
+
+
+    describe(`${CHEESE_NAME}`, () => {
+      it(`increase quality by 1 if quality < 50`, () => {
+        const item = {
+          name: CHEESE_NAME, 
+          quality: QUALITY_0,
+        }
+
+        const actual = updateQuality(item).quality
+
+        expect(actual).toBe(QUALITY_1)
+      })
+
+      it(`increase quality by 2 if sellIn < 0`, () => {
+        const item = {
+          name: CHEESE_NAME, 
+          quality: QUALITY_0,
+          sellIn: QUALITY_LT_0,
+        }
+
+        const actual = updateQuality(item).quality
+
+        expect(actual).toBe(QUALITY_2)
+      })
+    })
+
+    describe(`${CONJURED}`, () => {
+      it(`decrease quality by 2`, () => {
+        const item = {
+          name: CONJURED,
+          quality: QUALITY_2, 
+        }
+
+        const actual = updateQuality(item).quality
+
+        expect(actual).toBe(QUALITY_0)
+      })
+    })
+
+    describe(`${TICKET_NAME}`, () => {
+      const ticket = {
+        name: TICKET_NAME,
+        quality: QUALITY_0,
+        sellIn: SELL_IN_GT_TEN,
+      }
+
+
+      it(`increase quality by 1 if sellIn > 10`, () => {
+        const item = { ...ticket }
+
+        const actual = updateQuality(item).quality
+
+        expect(actual).toBe(QUALITY_1)
+      })
+
+
+      it(`increase quality by 2 if 11 > sellIn > 6`, () => {
+        const item = {
+          ...ticket,
+          sellIn: SELL_IN_BETWEEN_11_AND_6,
+        }
+
+        const actual = updateQuality(item).quality
+
+        expect(actual).toBe(QUALITY_2)
+      })
+
+
+      it(`increase quality by 3 if sellIn < 6`, () => {
+        const item = {
+          ...ticket,
+          sellIn: SELL_IN_LT_6,
+        }
+
+        const actual = updateQuality(item).quality
+
+        expect(actual).toBe(QUALITY_3)
+      })
+
+
+      it(`set quality to 0 if sellIn < 0`, () => {
+        const item = {
+          ...ticket,
+          quality: QUALITY_10,
+          sellIn: SELL_IN_LT_ZERO,
+        }
+
+        const actual = updateQuality(item).quality
+
+        expect(actual).toBe(QUALITY_0)
+      })
+    })
+  })
+
+  describe('sellIn mechanism', () => {
+    it(`decrease item sellIn for every item except ${SULFURAS}`, () => {
+      const item = { sellIn: SELL_IN_1 }
+
+      const actual = updateQuality(item).sellIn
+
+      expect(actual).toBe(SELL_IN_ZERO)
+    })
+  })
+
+  describe(`${SULFURAS}`, () => {
+    it(`leaves quality untouched`, () => {
+      const item = {
+        name: SULFURAS, 
+        quality: QUALITY_GT_0,
+      }
+
+      const actual = updateQuality(item).quality
+
+      expect(actual).toBe(QUALITY_GT_0)
+    })
+
+    it(`leaves sellIn untouched`, () => {
+      const item = {
+        name: SULFURAS,
+        sellIn: SELL_IN_1,
+      }
+
+      const actual = updateQuality(item).sellIn
+
+      expect(actual).toBe(SELL_IN_1)
+    })
+
+  })
 });
--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -1,3 +1,19 @@
+import {
+  lensProp,
+  over,
+  subtract,
+  inc,
+  propEq,
+  dec,
+  clamp,
+  identity,
+  cond,
+  T,
+  always,
+  compose,
+  pipe,
+} from 'ramda'
+
 export class Item {
   constructor(name, sellIn, quality){
     this.name = name;
@@ -6,55 +22,100 @@ export class Item {
   }
 }
 
+
+export const TICKET_NAME = 'Backstage passes to a TAFKAL80ETC concert'
+export const CHEESE_NAME = 'Aged Brie'
+export const SULFURAS = 'Sulfuras, Hand of Ragnaros'
+export const CONJURED = 'Conjured Mana Cake'
+
+export const QUALITY = 'quality'
+export const SELL_IN = 'sellIn'
+export const NAME = 'name'
+
+// helpers, utils
+export const nameEqual = propEq(NAME)
+export const isCheese = nameEqual(CHEESE_NAME)
+export const isTicket = nameEqual(TICKET_NAME)
+export const isSulfuras = nameEqual(SULFURAS)
+export const isConjured = nameEqual(CONJURED)
+
+export const qualityLens = lensProp(QUALITY) 
+export const overQuality = over(qualityLens)
+
+export const qualityBoundaries = clamp(0, 50) 
+export const increaseQuality = overQuality(inc)
+export const decreaseQuality = overQuality(dec)
+export const decreaseQualityBy2 = over(qualityLens, subtract(2))
+export const boundQuality = overQuality(qualityBoundaries)
+
+export const sellInLens = lensProp(SELL_IN)
+export const decreaseSellIn = over(sellInLens, dec)
+
+export const increaseQualityAfterSellDate = (item) => {
+  return item.sellIn < 0 ? increaseQuality(item) : item
+}
+export const decreaseQualityAfterSellDate = (item) => {
+  return item.sellIn < 0 ? decreaseQuality(item) : item
+}
+export const increaseQualityFor10DaysBeforeSellDate = (item) => {
+  return item.sellIn < 11 ? increaseQuality(item) : item
+}
+export const increaseQualityFor6DaysBeforeSellDate = (item) => {
+  return item.sellIn < 6 ? increaseQuality(item) : item
+}
+export const dropQualityToZeroAfterSellDate = (item) => {
+  return item.sellIn < 0 ? overQuality(always(0), item) : item
+}
+
+// specific behavior
+export const updateTicketQuality = compose(
+  increaseQualityFor6DaysBeforeSellDate,
+  increaseQualityFor10DaysBeforeSellDate
+)
+
+// item updates
+export const updateTicket = compose(
+  decreaseSellIn,
+  boundQuality,
+  dropQualityToZeroAfterSellDate,
+  updateTicketQuality,
+  increaseQuality
+)
+export const updateCommonItem = compose(
+  decreaseSellIn,
+  boundQuality,
+  decreaseQualityAfterSellDate,
+  decreaseQuality,
+)
+export const updateConjuredItem = compose(
+  decreaseSellIn,
+  boundQuality,  
+  decreaseQualityBy2
+)
+export const updateCheese = pipe(
+  increaseQuality,
+  increaseQualityAfterSellDate,
+  boundQuality,
+  decreaseSellIn
+)
+export const updateSulfuras = identity
+
+// why no pattern matching? :)
+export const updateQuality = cond([
+  [isConjured, updateConjuredItem],
+  [isSulfuras, updateSulfuras],
+  [isCheese, updateCheese],
+  [isTicket, updateTicket],
+  [T, updateCommonItem],
+])
+
 export class Shop {
   constructor(items=[]){
     this.items = items;
   }
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-        }
-      }
+      this.items[i] = updateQuality(this.items[i])
     }
 
     return this.items;
