f1183f3af0a72f8075497726befef0c30a0257c4
Marek Tobias <tobi@Mareks-MacBook-Pro.local>
2017-11-09 15:08:54 +0100

    Second attempt

--- src/__tests__/gilded_rose_spec.js
+++ src/__tests__/gilded_rose_spec.js
@@ -1,218 +1,263 @@
 import {
-  TICKET_NAME,
-  CHEESE_NAME,
-  SULFURAS,
-  CONJURED,
+    TICKET_NAME,
+    CHEESE_NAME,
+    SULFURAS,
+    CONJURED,
 
-  Shop,
-  Item,
+    Shop,
+    Item, decreaseQuality, increaseQuality, increaseQualityAfterSellDate, decreaseQualityAfterSellDate,
+    increaseQualityFor10DaysBeforeSellDate, increaseQualityFor6DaysBeforeSellDate, dropQualityToZeroAfterSellDate,
 } from '../gilded_rose';
 
-
-const QUALITY_GT_0 = 1
-const QUALITY_0 = 0
-const QUALITY_LT_0 = -1
-
-const QUALITY_GT_50 = 51
-const QUALITY_50 = 50
-
-const QUALITY_1 = 1
-const QUALITY_2 = 2
-const QUALITY_3 = 3
-const QUALITY_10 = 10
-
-
-const SELL_IN_1 = 1
-const SELL_IN_LT_ZERO = -1
-const SELL_IN_ZERO = 0
-
-// cheese
-const SELL_IN_GT_TEN = 11
-const SELL_IN_BETWEEN_11_AND_6 = 6
-const SELL_IN_LT_6 = 5
-
 const updateQuality = (item) => {
-  const shop = new Shop([new Item(item.name, item.sellIn, item.quality)])
-  return shop.updateQuality()[0]
+    const shop = new Shop([new Item(item.name, item.sellIn, item.quality)])
+    return shop.updateQuality()[0]
 }
 
-describe('Gilded Rose', function () {
-  describe('quality mechanism', () => {
-    describe('common behvaior', () => {
+describe('Gilded Rose Helpers/Utilities', () => {
+    describe('decrease quality of item older it gets', () => {
+        describe('decreaseQualityAfterSellDate', () => {
+            it('decrease quality when sellIn < 0', () => {
+                const item = {
+                    sellIn: -1,
+                    quality: 0,
+                }
+                const actual = decreaseQualityAfterSellDate(item).quality
+                expect(actual).toBe(-1)
+            })
 
-      it('decrease every item quality by 1 if quality > 0', () => {
-        const item = { quality: QUALITY_GT_0 }
+            it('leaves quality untouched when sellIn >= 0', () => {
+                const item = {
+                    sellIn: 0,
+                    quality: 0,
+                }
+                const actual = decreaseQualityAfterSellDate(item).quality
+                expect(actual).toBe(0)
+            })
+        })
 
-        const actual = updateQuality(item).quality
+        describe('dropQualityToZeroAfterSellDate', () => {
+            it('decrease quality when sellIn < 0', () => {
+                const item = {
+                    sellIn: -1,
+                    quality: 1,
+                }
+                const actual = dropQualityToZeroAfterSellDate(item).quality
+                expect(actual).toBe(0)
+            })
 
-        expect(actual).toBe(QUALITY_0)
-      })
-
-      it('bounds minimum quality to 0', () => {
-        const item = { quality: QUALITY_LT_0 }
-
-        const actual = updateQuality(item).quality
-
-        expect(actual).toBe(QUALITY_0)
-      })
-
-      it('bounds quality maximum to 50', () => {
-        const item = { quality: QUALITY_GT_50 }
-
-        const actual = updateQuality(item).quality
-
-        expect(actual).toBe(QUALITY_50)
-      })
-
-      it(`set quality to 0 if sellIn < 0`, () => {
-        const item = { 
-          quality: QUALITY_GT_0,
-          sellIn: SELL_IN_LT_ZERO,
-        }
-
-        const actual = updateQuality(item).quality
-
-        expect(actual).toBe(QUALITY_0)
-      })
-      
-      it(`decrease quality by 2 if sellIn < 0`, () => {
-        const item = { 
-          quality: QUALITY_2,
-          sellIn: SELL_IN_LT_ZERO,
-        }
-
-        const actual = updateQuality(item).quality
-
-        expect(actual).toBe(QUALITY_0)
-      })
+            it('leaves quality untouched when sellIn > 0', () => {
+                const item = {
+                    sellIn: 0,
+                    quality: 1,
+                }
+                const actual = dropQualityToZeroAfterSellDate(item).quality
+                expect(actual).toBe(1)
+            })
+        })
     })
 
+    describe('increase quality of item older it gets', () => {
+        describe('increaseQualityAfterSellDate', () => {
+            it('increases quality when sellIn < 0', () => {
+                const item = {
+                    sellIn: -1,
+                    quality: 0,
+                }
+                const actual = increaseQualityAfterSellDate(item).quality
+                expect(actual).toBe(1)
+            })
 
-    describe(`${CHEESE_NAME}`, () => {
-      it(`increase quality by 1 if quality < 50`, () => {
-        const item = {
-          name: CHEESE_NAME, 
-          quality: QUALITY_0,
-        }
+            it('leaves quality untouched when sellIn >= 0', () => {
+                const item = {
+                    sellIn: 0,
+                    quality: 0,
+                }
+                const actual = increaseQualityAfterSellDate(item).quality
+                expect(actual).toBe(0)
+            })
+        })
 
-        const actual = updateQuality(item).quality
+        describe('increaseQualityFor10DaysBeforeSellDate by 1', () => {
+            it('increases quality when sellIn is < 11', () => {
+                const item = {
+                    sellIn: 10,
+                    quality: 0,
+                }
+                const actual = increaseQualityFor10DaysBeforeSellDate(item).quality
+                expect(actual).toBe(1)
+            })
 
-        expect(actual).toBe(QUALITY_1)
-      })
+            it('leaves quality untouched if sellIn is 11', () => {
+                const item = {
+                    sellIn: 11,
+                    quality: 0,
+                }
+                const actual = increaseQualityFor10DaysBeforeSellDate(item).quality
+                expect(actual).toBe(0)
+            })
+        })
 
-      it(`increase quality by 2 if sellIn < 0`, () => {
-        const item = {
-          name: CHEESE_NAME, 
-          quality: QUALITY_0,
-          sellIn: QUALITY_LT_0,
-        }
+        describe('increaseQualityFor6DaysBeforeSellDate by 1', () => {
+            it('increases quality when sellIn is < 6', () => {
+                const item = {
+                    sellIn: 5,
+                    quality: 0,
+                }
+                const actual = increaseQualityFor6DaysBeforeSellDate(item).quality
+                expect(actual).toBe(1)
+            })
 
-        const actual = updateQuality(item).quality
+            it('leaves quality untouched if sellIn >= 6', () => {
+                const item = {
+                    sellIn: 6,
+                    quality: 0,
+                }
+                const actual = increaseQualityFor6DaysBeforeSellDate(item).quality
+                expect(actual).toBe(0)
+            })
+        })
+    })
+})
 
-        expect(actual).toBe(QUALITY_2)
-      })
+describe('Gilded Rose Behavior', () => {
+    describe('general', () => {
+        it('decrease sellIn ', () => {
+            const item = {
+                sellIn: 0,
+            }
+            const actual = updateQuality(item).sellIn
+            expect(actual).toBe(-1)
+        })
+
+        it('boundQuality minimum to 0', () => {
+            const item = {
+                quality: -1,
+            }
+            const actual = updateQuality(item).quality
+            expect(actual).toBe(0)
+        })
+
+        it('boundQuality maximum to 50', () => {
+            const item = {
+                quality: 51,
+            }
+            const actual = updateQuality(item).quality
+            expect(actual).toBe(50)
+        })
     })
 
-    describe(`${CONJURED}`, () => {
-      it(`decrease quality by 2`, () => {
-        const item = {
-          name: CONJURED,
-          quality: QUALITY_2, 
-        }
+    describe('item specific', () => {
+        describe('common item', () => {
+            it('decrease quality by 1 before sell date', () => {
+                const item = {
+                    sellIn: 0,
+                    quality: 1,
+                }
+                const actual = updateQuality(item).quality
+                expect(actual).toBe(0)
+            })
 
-        const actual = updateQuality(item).quality
+            it('decrease quality by 2 after sell date', () => {
+                const item = {
+                    sellIn: -1,
+                    quality: 2,
+                }
+                const actual = updateQuality(item).quality
+                expect(actual).toBe(0)
+            })
+        })
 
-        expect(actual).toBe(QUALITY_0)
-      })
+        describe(`${TICKET_NAME}`, () => {
+            it('increases quality by 1 if sellIn >= 11', () => {
+                const item = {
+                    name: TICKET_NAME,
+                    sellIn: 11,
+                    quality: 0,
+                }
+                const actual = updateQuality(item).quality
+                expect(actual).toBe(1)
+            })
+
+            it('increases quality by 2 if sellIn < 11', () => {
+                const item = {
+                    name: TICKET_NAME,
+                    sellIn: 10,
+                    quality: 0,
+                }
+                const actual = updateQuality(item).quality
+                expect(actual).toBe(2)
+            })
+
+            it('increases quality by 3 if sellIn < 6', () => {
+                const item = {
+                    name: TICKET_NAME,
+                    sellIn: 5,
+                    quality: 0,
+                }
+                const actual = updateQuality(item).quality
+                expect(actual).toBe(3)
+            })
+
+            it('set quality to 0 if sellIn < 0', () => {
+                const item = {
+                    name: TICKET_NAME,
+                    sellIn: -1,
+                    quality: 10,
+                }
+                const actual = updateQuality(item).quality
+                expect(actual).toBe(0)
+            })
+        })
+
+        describe(`${CHEESE_NAME}`, () => {
+            it('increase quality by 1', () => {
+                const item = {
+                    name: CHEESE_NAME,
+                    sellIn: 0,
+                    quality: 0,
+                }
+                const actual = updateQuality(item).quality
+                expect(actual).toBe(1)
+            })
+
+            it('increase quality by 2 if sellIn < 0', () => {
+                const item = {
+                    name: CHEESE_NAME,
+                    sellIn: -1,
+                    quality: 0,
+                }
+                const actual = updateQuality(item).quality
+                expect(actual).toBe(2)
+            })
+        })
+
+        describe(`${SULFURAS}`, () => {
+            it(`leaves ${SULFURAS} untouched (pointer equals)`, () => {
+                const sulfuras = new Item(SULFURAS, 0, 0)
+                const shop = new Shop([sulfuras])
+                const actual = shop.updateQuality()[0]
+                expect(actual).toBe(sulfuras)
+            })
+
+            it(`leaves ${SULFURAS} untouched (value equals)`, () => {
+                const item = {name: SULFURAS}
+                const actual = updateQuality(item)
+                expect(actual).toEqual(item)
+            })
+        })
+
+        describe(`${CONJURED}`, () => {
+            it('decrease quality by 2', () => {
+                const item = {
+                    name: CONJURED,
+                    sellIn: 0,
+                    quality: 2,
+                }
+                const actual = updateQuality(item).quality
+                expect(actual).toBe(0)
+            })
+        })
+
     })
-
-    describe(`${TICKET_NAME}`, () => {
-      const ticket = {
-        name: TICKET_NAME,
-        quality: QUALITY_0,
-        sellIn: SELL_IN_GT_TEN,
-      }
-
-
-      it(`increase quality by 1 if sellIn > 10`, () => {
-        const item = { ...ticket }
-
-        const actual = updateQuality(item).quality
-
-        expect(actual).toBe(QUALITY_1)
-      })
-
-
-      it(`increase quality by 2 if 11 > sellIn > 6`, () => {
-        const item = {
-          ...ticket,
-          sellIn: SELL_IN_BETWEEN_11_AND_6,
-        }
-
-        const actual = updateQuality(item).quality
-
-        expect(actual).toBe(QUALITY_2)
-      })
-
-
-      it(`increase quality by 3 if sellIn < 6`, () => {
-        const item = {
-          ...ticket,
-          sellIn: SELL_IN_LT_6,
-        }
-
-        const actual = updateQuality(item).quality
-
-        expect(actual).toBe(QUALITY_3)
-      })
-
-
-      it(`set quality to 0 if sellIn < 0`, () => {
-        const item = {
-          ...ticket,
-          quality: QUALITY_10,
-          sellIn: SELL_IN_LT_ZERO,
-        }
-
-        const actual = updateQuality(item).quality
-
-        expect(actual).toBe(QUALITY_0)
-      })
-    })
-  })
-
-  describe('sellIn mechanism', () => {
-    it(`decrease item sellIn for every item except ${SULFURAS}`, () => {
-      const item = { sellIn: SELL_IN_1 }
-
-      const actual = updateQuality(item).sellIn
-
-      expect(actual).toBe(SELL_IN_ZERO)
-    })
-  })
-
-  describe(`${SULFURAS}`, () => {
-    it(`leaves quality untouched`, () => {
-      const item = {
-        name: SULFURAS, 
-        quality: QUALITY_GT_0,
-      }
-
-      const actual = updateQuality(item).quality
-
-      expect(actual).toBe(QUALITY_GT_0)
-    })
-
-    it(`leaves sellIn untouched`, () => {
-      const item = {
-        name: SULFURAS,
-        sellIn: SELL_IN_1,
-      }
-
-      const actual = updateQuality(item).sellIn
-
-      expect(actual).toBe(SELL_IN_1)
-    })
-
-  })
-});
+})
--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -1,25 +1,25 @@
 import {
-  lensProp,
-  over,
-  subtract,
-  inc,
-  propEq,
-  dec,
-  clamp,
-  identity,
-  cond,
-  T,
-  always,
-  compose,
-  pipe,
+    lensProp,
+    over,
+    subtract,
+    inc,
+    propEq,
+    dec,
+    clamp,
+    cond,
+    always,
+    compose,
+    allPass,
+    ifElse,
+    identity,
 } from 'ramda'
 
 export class Item {
-  constructor(name, sellIn, quality){
-    this.name = name;
-    this.sellIn = sellIn;
-    this.quality = quality;
-  }
+    constructor(name, sellIn, quality) {
+        this.name = name;
+        this.sellIn = sellIn;
+        this.quality = quality;
+    }
 }
 
 
@@ -38,11 +38,12 @@ export const isCheese = nameEqual(CHEESE_NAME)
 export const isTicket = nameEqual(TICKET_NAME)
 export const isSulfuras = nameEqual(SULFURAS)
 export const isConjured = nameEqual(CONJURED)
+export const isCommonItem = (item) => !allPass[isConjured, isSulfuras, isCheese, isTicket]
 
-export const qualityLens = lensProp(QUALITY) 
+export const qualityLens = lensProp(QUALITY)
 export const overQuality = over(qualityLens)
 
-export const qualityBoundaries = clamp(0, 50) 
+export const qualityBoundaries = clamp(0, 50)
 export const increaseQuality = overQuality(inc)
 export const decreaseQuality = overQuality(dec)
 export const decreaseQualityBy2 = over(qualityLens, subtract(2))
@@ -52,72 +53,66 @@ export const sellInLens = lensProp(SELL_IN)
 export const decreaseSellIn = over(sellInLens, dec)
 
 export const increaseQualityAfterSellDate = (item) => {
-  return item.sellIn < 0 ? increaseQuality(item) : item
+    return item.sellIn < 0 ? increaseQuality(item) : item
 }
 export const decreaseQualityAfterSellDate = (item) => {
-  return item.sellIn < 0 ? decreaseQuality(item) : item
+    return item.sellIn < 0 ? decreaseQuality(item) : item
 }
 export const increaseQualityFor10DaysBeforeSellDate = (item) => {
-  return item.sellIn < 11 ? increaseQuality(item) : item
+    return item.sellIn < 11 ? increaseQuality(item) : item
 }
 export const increaseQualityFor6DaysBeforeSellDate = (item) => {
-  return item.sellIn < 6 ? increaseQuality(item) : item
+    return item.sellIn < 6 ? increaseQuality(item) : item
 }
 export const dropQualityToZeroAfterSellDate = (item) => {
-  return item.sellIn < 0 ? overQuality(always(0), item) : item
+    return item.sellIn < 0 ? overQuality(always(0), item) : item
 }
 
-// specific behavior
-export const updateTicketQuality = compose(
-  increaseQualityFor6DaysBeforeSellDate,
-  increaseQualityFor10DaysBeforeSellDate
-)
 
 // item updates
-export const updateTicket = compose(
-  decreaseSellIn,
-  boundQuality,
-  dropQualityToZeroAfterSellDate,
-  updateTicketQuality,
-  increaseQuality
-)
 export const updateCommonItem = compose(
-  decreaseSellIn,
-  boundQuality,
-  decreaseQualityAfterSellDate,
-  decreaseQuality,
+    decreaseQualityAfterSellDate,
+    decreaseQuality,
 )
-export const updateConjuredItem = compose(
-  decreaseSellIn,
-  boundQuality,  
-  decreaseQualityBy2
+export const updateTicket = compose(
+    dropQualityToZeroAfterSellDate,
+    increaseQualityFor6DaysBeforeSellDate,
+    increaseQualityFor10DaysBeforeSellDate,
+    increaseQuality
 )
-export const updateCheese = pipe(
-  increaseQuality,
-  increaseQualityAfterSellDate,
-  boundQuality,
-  decreaseSellIn
+export const updateCheese = compose(
+    increaseQualityAfterSellDate,
+    increaseQuality,
 )
-export const updateSulfuras = identity
+export const updateConjuredItem = item => {
+   return isConjured(item) ? decreaseQualityBy2(item) : item
+}
 
-// why no pattern matching? :)
+// general
 export const updateQuality = cond([
-  [isConjured, updateConjuredItem],
-  [isSulfuras, updateSulfuras],
-  [isCheese, updateCheese],
-  [isTicket, updateTicket],
-  [T, updateCommonItem],
+    [isConjured, updateConjuredItem],
+    [isCheese, updateCheese],
+    [isTicket, updateTicket],
+    [isCommonItem, updateCommonItem],
 ])
 
+export const update = compose(
+    decreaseSellIn,
+    boundQuality,
+    updateQuality
+)
+
 export class Shop {
-  constructor(items=[]){
-    this.items = items;
-  }
-  updateQuality() {
-    for (var i = 0; i < this.items.length; i++) {
-      this.items[i] = updateQuality(this.items[i])
+    constructor(items = []) {
+        this.items = items;
     }
 
-    return this.items;
-  }
+    updateQuality() {
+        for (let i = 0; i < this.items.length; i++) {
+            const item = this.items[i]
+            this.items[i] = isSulfuras(item) ? item : update(item)
+        }
+
+        return this.items;
+    }
 }
