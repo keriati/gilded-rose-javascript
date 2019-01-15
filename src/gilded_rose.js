c606238ab92d56dc5b65a7421ec8291bc1d77fbf
Michal Ševčík <sevcik.mi@gmail.com>
2019-01-15 10:57:18 +0100

    Initial code review.

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -1,5 +1,5 @@
 export class Item {
-  constructor(name, sellIn, quality){
+  constructor(name, sellIn, quality) {
     this.name = name;
     this.sellIn = sellIn;
     this.quality = quality;
@@ -7,50 +7,95 @@ export class Item {
 }
 
 export class Shop {
-  constructor(items=[]){
+  constructor(items = []) {
     this.items = items;
   }
+
+  // So I would like you to refactor this piece of code so that
+  // the updateQuality() function loops over the items, and based on given set of
+  // predicates it will (or will not) perform updates on the item
+  //
+  // use something like:
+  // addModification(predicateFn, updateFn)
   updateQuality() {
+    // Replace for loop with a functional array.map
     for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+      // create a variable that holds the currently processed item (const item = ....)
+      // use && instead of nesting conditions
+      if (
+        // use strict equal comparison
+        this.items[i].name != "Aged Brie" &&
+        // use strict equal comparison
+        this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
+      ) {
+        // use && instead of nesting conditions
         if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+          // use strict equal comparison
+          if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+            // create and use function decreaseQuality(item)
             this.items[i].quality = this.items[i].quality - 1;
           }
         }
       } else {
+        // use && instead of nesting conditions
         if (this.items[i].quality < 50) {
+          // create and use function increaseQuality(item)
           this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+          // use && instead of nesting conditions
+          if (
+            // use strict equal comparison
+            this.items[i].name == "Backstage passes to a TAFKAL80ETC concert"
+          ) {
+            // use && instead of nesting conditions
             if (this.items[i].sellIn < 11) {
+              // use && instead of nesting conditions
               if (this.items[i].quality < 50) {
+                // create and use function increaseQuality(item)
                 this.items[i].quality = this.items[i].quality + 1;
               }
             }
             if (this.items[i].sellIn < 6) {
               if (this.items[i].quality < 50) {
+                // create and use function increaseQuality(item)
                 this.items[i].quality = this.items[i].quality + 1;
               }
             }
           }
         }
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+      // use strict equal comparison
+      if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+        // create and use function decreaseSellIn(item)
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
+      // use && instead of nesting conditions
+      // create and use a function that tells me that the product is expired - e.g. isExpired(item)
       if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+        // use strict equal comparison
+        // use && instead of nesting conditions
+        if (this.items[i].name != "Aged Brie") {
+          // use && instead of nesting conditions
+          if (
+            // use strict equal comparison
+            this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
+          ) {
+            // use && instead of nesting conditions
             if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+              // use && instead of nesting conditions
+              // use strict equal comparison
+              if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+                // create and use function decreaseQuality(item)
                 this.items[i].quality = this.items[i].quality - 1;
               }
             }
           } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
+            // create and use a function to set quality to 0, e.g. setWorthless(item)
+            this.items[i].quality =
+              this.items[i].quality - this.items[i].quality;
           }
         } else {
           if (this.items[i].quality < 50) {
+            // create and use function increaseQuality(item)
             this.items[i].quality = this.items[i].quality + 1;
           }
         }
