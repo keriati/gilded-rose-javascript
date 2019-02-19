0112463e3c62137d68b3029724ab35af183175e1
Alex <alexandretanner@surfer-172-30-100-224-hotspot.internet-for-guests.com>
2019-02-19 12:56:38 +0100

    added initial testing snapshot
    - added eslint & prettier
    - added initial snapshot test

--- src/__tests__/gilded_rose_spec.js
+++ src/__tests__/gilded_rose_spec.js
@@ -1,11 +1,34 @@
-import { Shop, Item } from '../gilded_rose';
+import { Shop, Item } from "../gilded_rose";
 
-describe("Gilded Rose", function () {
+describe("Gilded Rose", function() {
+  // it("should foo", function() {
+  //   const gildedRose = new Shop([new Item("foo", 0, 0)]);
+  //   const items = gildedRose.updateQuality();
+  //   expect(items[0].name).toEqual("fixme");
+  // });
 
-    it("should foo", function () {
-        const gildedRose = new Shop([new Item("foo", 0, 0)]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).toEqual("fixme");
-    });
+  it("should not break the original snapshot (without conjured)", () => {
+    const items = [];
 
+    items.push(new Item("+5 Dexterity Vest", 10, 20));
+    items.push(new Item("Aged Brie", 2, 0));
+
+    items.push(new Item("Elixir of the Mongoose", 5, 7));
+    items.push(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
+    items.push(new Item("Sulfuras, Hand of Ragnaros", -1, 80));
+    items.push(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
+    items.push(new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49));
+    items.push(new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49));
+    // this conjured item does not work properly yet
+    items.push(new Item("Conjured Mana Cake", 3, 6));
+
+    const gildedRose = new Shop(items);
+
+    const days = 50;
+
+    for (let i = 0; i < days; i++) {
+      gildedRose.updateQuality();
+      expect(gildedRose.items).toMatchSnapshot();
+    }
+  });
 });
