45831cb8f0a71a57b9fff71ba7a639a10b88e009
Tomas Jerry Sebo <sebo.tomas@gmail.com>
2019-03-12 15:09:03 +0100

    Refactoring Shop class using switch statement based on item type

--- src/modules/Shop.js
+++ src/modules/Shop.js
@@ -4,50 +4,49 @@ class Shop {
   }
 
   increaseQuality(item) {
+    if (item.quality === 50) return;
     item.quality++;
   }
   decreaseQuality(item) {
+    if (item.quality === 0) return;
     item.quality--;
   }
   updateBackstagePass(item) {
-    if (item.quality < 50) {
+    this.increaseQuality(item);
+    if (item.sellIn < 11) {
       this.increaseQuality(item);
-      if (item.sellIn < 11) {
-        if (item.quality < 50) {
-          this.increaseQuality(item);
-        }
-      }
-      if (item.sellIn < 6) {
-        if (item.quality < 50) {
-          this.increaseQuality(item);
-        }
-      }
     }
+    if (item.sellIn < 6) {
+      this.increaseQuality(item);
+    }
+
     item.sellIn = item.sellIn - 1;
     if (item.sellIn < 0) {
       item.quality = item.quality - item.quality;
     }
   }
   updateAgedBrie(item) {
-    if (item.quality < 50) {
-      this.increaseQuality(item);
-    }
+    this.increaseQuality(item);
+
     item.sellIn = item.sellIn - 1;
     if (item.sellIn < 0) {
-      if (item.quality < 50) {
-        this.increaseQuality(item);
-      }
+      this.increaseQuality(item);
     }
   }
   updateRegularItem(item) {
-    if (item.quality > 0) {
-      this.decreaseQuality(item);
-    }
+    this.decreaseQuality(item);
     item.sellIn = item.sellIn - 1;
     if (item.sellIn < 0) {
-      if (item.quality > 0) {
-        this.decreaseQuality(item);
-      }
+      this.decreaseQuality(item);
+    }
+  }
+  updateConjuredManaCake(item) {
+    this.decreaseQuality(item);
+    this.decreaseQuality(item);
+    item.sellIn = item.sellIn - 1;
+    if (item.sellIn < 0) {
+      this.decreaseQuality(item);
+      this.decreaseQuality(item);
     }
   }
   updateQuality() {
@@ -61,6 +60,10 @@ class Shop {
           break;
         case "Sulfuras, Hand of Ragnaros":
           break;
+        case "Conjured Mana Cake":
+          this.updateConjuredManaCake(item);
+          break;
+
         default:
           this.updateRegularItem(item);
           break;
