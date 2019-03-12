7f10fa48e488ec268551ceb24b6001623e951948
Tomas Jerry Sebo <sebo.tomas@gmail.com>
2019-03-12 14:46:02 +0100

    Refactoring Shop js using a switch statement based on item type

--- src/modules/Shop.js
+++ src/modules/Shop.js
@@ -3,71 +3,67 @@ class Shop {
     this.items = items;
   }
 
-  isRegularItem(name) {
-    if (
-      name !== "Aged Brie" &&
-      name !== "Backstage passes to a TAFKAL80ETC concert"
-    )
-      return true;
-    return false;
-  }
-
-  isBackStagePass(name) {
-    if (name === "Backstage passes to a TAFKAL80ETC concert") return true;
-    return false;
-  }
-
-  isSulfurasItem(name) {
-    if (name === "Sulfuras, Hand of Ragnaros") return true;
-    return false;
-  }
   increaseQuality(item) {
     item.quality++;
   }
   decreaseQuality(item) {
     item.quality--;
   }
-  updateQuality() {
-    this.items.forEach((item, key) => {
-      if (this.isSulfurasItem(item.name)) return;
-
-      if (this.isRegularItem(item.name)) {
-        if (item.quality > 0) {
-          this.decreaseQuality(item);
-        }
-      } else {
+  updateBackstagePass(item) {
+    if (item.quality < 50) {
+      this.increaseQuality(item);
+      if (item.sellIn < 11) {
         if (item.quality < 50) {
           this.increaseQuality(item);
-          if (this.isBackStagePass(item.name)) {
-            if (item.sellIn < 11) {
-              if (item.quality < 50) {
-                this.increaseQuality(item);
-              }
-            }
-            if (item.sellIn < 6) {
-              if (item.quality < 50) {
-                this.increaseQuality(item);
-              }
-            }
-          }
         }
       }
-      item.sellIn = item.sellIn - 1;
-
-      if (item.sellIn < 0) {
-        if (item.name != "Aged Brie") {
-          if (!this.isBackStagePass(item.name)) {
-            if (item.quality > 0) {
-              this.decreaseQuality(item);
-            }
-          } else {
-            item.quality = item.quality - item.quality;
-          }
-        } else {
-          if (item.quality < 50) {
-            this.increaseQuality(item);
-          }
+      if (item.sellIn < 6) {
+        if (item.quality < 50) {
+          this.increaseQuality(item);
         }
+      }
+    }
+    item.sellIn = item.sellIn - 1;
+    if (item.sellIn < 0) {
+      item.quality = item.quality - item.quality;
+    }
+  }
+  updateAgedBrie(item) {
+    if (item.quality < 50) {
+      this.increaseQuality(item);
+    }
+    item.sellIn = item.sellIn - 1;
+    if (item.sellIn < 0) {
+      if (item.quality < 50) {
+        this.increaseQuality(item);
+      }
+    }
+  }
+  updateRegularItem(item) {
+    if (item.quality > 0) {
+      this.decreaseQuality(item);
+    }
+    item.sellIn = item.sellIn - 1;
+    if (item.sellIn < 0) {
+      if (item.quality > 0) {
+        this.decreaseQuality(item);
+      }
+    }
+  }
+  updateQuality() {
+    this.items.forEach((item, key) => {
+      switch (item.name) {
+        case "Aged Brie":
+          this.updateAgedBrie(item);
+          break;
+        case "Backstage passes to a TAFKAL80ETC concert":
+          this.updateBackstagePass(item);
+          break;
+        case "Sulfuras, Hand of Ragnaros":
+          break;
+        default:
+          this.updateRegularItem(item);
+          break;
       }
     });
 
