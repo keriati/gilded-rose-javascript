336ae24ac187b01a51b7af7b1d1065ae7ccc5c2d
japutko <jakubsebok@gmail.com>
2020-01-14 14:20:50 +0100

    Extracts backstage passes

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -13,11 +13,35 @@ export class Shop {
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
       const item = this.items[i];
+
+      if (item.name.startsWith('Sulfuras')) {
+        continue;
+      }
+
+      if (item.name.startsWith('Backstage passes')) {
+        if (item.quality < 50) {
+          item.quality = item.quality + 1;
+          if (item.sellIn < 11) {
+            if (item.quality < 50) {
+              item.quality = item.quality + 1;
+            }
+          }
+          if (item.sellIn < 6) {
+            if (item.quality < 50) {
+              item.quality = item.quality + 1;
+            }
+          }
+        }
+        item.sellIn = item.sellIn - 1;
+        if (item.sellIn < 0) {
+          item.quality = item.quality - item.quality;
+        }
+        continue;
+      }
+
       if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (item.quality > 0) {
-          if (item.name != 'Sulfuras, Hand of Ragnaros') {
-            item.quality = item.quality - 1;
-          }
+          item.quality = item.quality - 1;
         }
       } else {
         if (item.quality < 50) {
@@ -36,16 +60,14 @@ export class Shop {
           }
         }
       }
-      if (item.name != 'Sulfuras, Hand of Ragnaros') {
-        item.sellIn = item.sellIn - 1;
-      }
+
+      item.sellIn = item.sellIn - 1;
+      
       if (item.sellIn < 0) {
         if (item.name != 'Aged Brie') {
           if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
             if (item.quality > 0) {
-              if (item.name != 'Sulfuras, Hand of Ragnaros') {
-                item.quality = item.quality - 1;
-              }
+              item.quality = item.quality - 1;
             }
           } else {
             item.quality = item.quality - item.quality;
