14f24688d4a21c174eb808588fe5afe844c1637d
japutko <jakubsebok@gmail.com>
2020-01-14 14:31:20 +0100

    Add Conjured items

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -39,44 +39,39 @@ export class Shop {
         continue;
       }
 
-      if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (item.quality > 0) {
-          item.quality = item.quality - 1;
-        }
-      } else {
+      if (item.name.startsWith('Aged Brie')) {
         if (item.quality < 50) {
           item.quality = item.quality + 1;
-          if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (item.sellIn < 11) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1;
-              }
-            }
-            if (item.sellIn < 6) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1;
-              }
-            }
-          }
         }
+
+        item.sellIn = item.sellIn - 1;
+        
+        if (item.sellIn < 0 && item.quality < 50) {
+          item.quality = item.quality + 1;
+        }
+        continue;
       }
 
-      item.sellIn = item.sellIn - 1;
-      
-      if (item.sellIn < 0) {
-        if (item.name != 'Aged Brie') {
-          if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (item.quality > 0) {
-              item.quality = item.quality - 1;
-            }
-          } else {
-            item.quality = item.quality - item.quality;
-          }
-        } else {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-          }
+      if (item.name.startsWith('Conjured')) {
+        if (item.quality > 0) {
+          item.quality = item.quality - 2;
         }
+        item.sellIn = item.sellIn - 1;
+        if (item.sellIn < 0 && item.quality > 0) {
+          item.quality = item.quality - 2;
+        }
+        if (item.quality < 0) {
+          item.quality = 0;
+        }
+        continue;
+      }
+
+      if (item.quality > 0) {
+        item.quality = item.quality - 1;
+      }
+      item.sellIn = item.sellIn - 1;
+      if (item.sellIn < 0 && item.quality > 0) {
+        item.quality = item.quality - 1;
       }
     }
 
