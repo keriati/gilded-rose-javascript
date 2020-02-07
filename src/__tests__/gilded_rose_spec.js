469baa2d6981498644b8ec2d868a207ea72c4305
Milan Slancik <milan.slancik@gmail.com>
2020-02-07 15:11:15 +0000

    SILVIU added more expects

--- src/__tests__/gilded_rose_spec.js
+++ src/__tests__/gilded_rose_spec.js
@@ -14,16 +14,19 @@ describe("Gilded Rose", function () {
             const gildedRose = new Shop([new Item("generic", SELL_IN_EXPIRED, QUALITY_MIN)]);
             const items = gildedRose.updateQuality();
             expect(items[0].quality).toBe(0);
+            expect(items[0].sellIn).toBe(-1);
         });
         it("quality should decrease by 1", function () {
             const gildedRose = new Shop([new Item("generic", 0, 1)]);
             const items = gildedRose.updateQuality();
             expect(items[0].quality).toBe(0);
+            expect(items[0].sellIn).toBe(-1);
         });
         it("quality should degrade twice as fast when expires", () => {
             const gildedRose = new Shop([new Item("generic", -1, 10)]);
             const items = gildedRose.updateQuality();
             expect(items[0].quality).toBe(8);
+            expect(items[0].sellIn).toBe(-2);
         });
     });
 
@@ -32,12 +35,14 @@ describe("Gilded Rose", function () {
             const gildedRose = new Shop([new Item("Aged Brie", 10, 30)]);
             const items = gildedRose.updateQuality();
             expect(items[0].quality).toBe(31);
+            expect(items[0].sellIn).toBe(9);
         });
 
         it("quality is never more than 50", function () {
             const gildedRose = new Shop([new Item("Aged Brie", 10, 50)]);
             const items = gildedRose.updateQuality();
             expect(items[0].quality).toBe(50);
+            expect(items[0].sellIn).toBe(9);
         });
 
         
@@ -45,12 +50,14 @@ describe("Gilded Rose", function () {
             const gildedRose = new Shop([new Item("Aged Brie", -2, 40)]);
             const items = gildedRose.updateQuality();
             expect(items[0].quality).toBe(42);
+            expect(items[0].sellIn).toBe(-3);
         });
 
         it("quality doesn't increase when is 50 and more", function () {
             const gildedRose = new Shop([new Item("Aged Brie", -2, 49)]);
             const items = gildedRose.updateQuality();
             expect(items[0].quality).toBe(50);
+            expect(items[0].sellIn).toBe(-3);
         });
     });
 
@@ -66,6 +73,7 @@ describe("Gilded Rose", function () {
             const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", -1, 1)]);
             const items = gildedRose.updateQuality();
             expect(items[0].quality).toBe(1);
+            expect(items[0].sellIn).toBe(-1);
         });
     });
 
@@ -75,30 +83,35 @@ describe("Gilded Rose", function () {
             const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 20, 30)]);
             const items = gildedRose.updateQuality();
             expect(items[0].quality).toBe(31);
+            expect(items[0].sellIn).toBe(19);
         });
 
         it("increases in Quality by 2 as SellIn value is 10 days or less", () => {
             const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 10, 30)]);
             const items = gildedRose.updateQuality();
             expect(items[0].quality).toBe(32);
+            expect(items[0].sellIn).toBe(9);
         });
 
         it("quality should be the same when it reaches 50 & SellIn is 10 days or less", () => {
             const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49)]);
             const items = gildedRose.updateQuality();
             expect(items[0].quality).toBe(50);
+            expect(items[0].sellIn).toBe(9);
         });
 
         it("increases in Quality by 3 as SellIn value is 5 days or less", () => {
             const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 5, 20)]);
             const items = gildedRose.updateQuality();
             expect(items[0].quality).toBe(23);
+            expect(items[0].sellIn).toBe(4);
         });
 
         it("quality should be the same when it reaches 50 & SellIn value is 5 days or less", () => {
             const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49)]);
             const items = gildedRose.updateQuality();
             expect(items[0].quality).toBe(50);
+            expect(items[0].sellIn).toBe(4);
         });
 
 
@@ -106,6 +119,7 @@ describe("Gilded Rose", function () {
             const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 0, 10)]);
             const items = gildedRose.updateQuality();
             expect(items[0].quality).toBe(0);
+            expect(items[0].sellIn).toBe(-1);
         });
 
     });
