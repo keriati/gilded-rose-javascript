065b62179e2832a609270181069de024b0ab1086
Attila Kerekes <attila.kerekes@erstegroup.com>
2020-02-07 13:56:39 +0100

    Adding tests

--- src/__tests__/gilded_rose_spec.js
+++ src/__tests__/gilded_rose_spec.js
@@ -2,10 +2,100 @@ import { Shop, Item } from '../gilded_rose';
 
 describe("Gilded Rose", function () {
 
-    it("should foo", function () {
-        const gildedRose = new Shop([new Item("foo", 0, 0)]);
+    it("Tests if item name is still the same", function () {
+        const gildedRose = new Shop([new Item("fhammer", 0, 0)]);
         const items = gildedRose.updateQuality();
-        expect(items[0].name).toEqual("fixme");
+        expect(items[0].name).toEqual("fhammer");
+    });
+   
+    it("Tests if quality doesnt fall below zero", function () {
+        const gildedRose = new Shop([new Item("fhammer", 0, 0)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).toEqual(0);
+    });
+
+    it("Tests if quality does decrace by one", function () {
+        const gildedRose = new Shop([new Item("fhammer", 2, 4)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).toEqual(3);
+    });
+
+    it("Tests if quality decreases by 2 when sellIn Date has passed", function () {
+        const gildedRose = new Shop([new Item("fhammer", 0, 4)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).toEqual(2);
+    });
+
+    it("Tests if the sellIn value decreases by one", function () {
+        const gildedRose = new Shop([new Item("fhammer", 0, 0)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].sellIn).toEqual(-1);
+    });
+
+    describe("Aged Brie", function () {
+        it("Tests if the quality value increases by one", function () {
+            const gildedRose = new Shop([new Item("Aged Brie", 1, 0)]);
+            const items = gildedRose.updateQuality();
+            expect(items[0].quality).toEqual(+1);
+        });
+
+        it("Tests if the quality value increases by two if SellIn == 0", function () {
+            const gildedRose = new Shop([new Item("Aged Brie", 0, 0)]);
+            const items = gildedRose.updateQuality();
+            expect(items[0].quality).toEqual(2);
+        });
+
+        it("Tests if the quality value can not be greater than 50", function () {
+            const gildedRose = new Shop([new Item("Aged Brie", 1, 50)]);
+            const items = gildedRose.updateQuality();
+            expect(items[0].quality).toEqual(50);
+        });
+    });
+
+    describe("Sulfuras, Hand of Ragnaros", function () {
+        it("Tests if the quality value doesnt decrease", function () {
+            const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 1, 17)]);
+            const items = gildedRose.updateQuality();
+            expect(items[0].quality).toEqual(17);
+        });
+
+        it("Tests if the quality value doesnt decrease twice as fast when SellIn == 0", function () {
+            const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 0, 17)]);
+            const items = gildedRose.updateQuality();
+            expect(items[0].quality).toEqual(17);
+        });
+
+        it("Tests if the SellIn value doesnt change", function () {
+            const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 1, 17)]);
+            const items = gildedRose.updateQuality();
+            expect(items[0].sellIn).toEqual(1);
+        });
+    });
+
+    describe("Backstage passes to a TAFKAL80ETC concert", function () {
+        it("Tests if the quality value increases by 1 when sellIn >10", function () {
+            const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 20, 10)]);
+            const items = gildedRose.updateQuality();
+            expect(items[0].quality).toEqual(11);
+        });
+
+        it("Tests if the quality value increases by 2 when 5 < sellIn <= 10", function () {
+            const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20)]);
+            const items = gildedRose.updateQuality();
+            expect(items[0].quality).toEqual(22);
+        });
+
+        it("Tests if the quality value increases by 3 when 0 < sellIn <= 5", function () {
+            const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 5, 20)]);
+            const items = gildedRose.updateQuality();
+            expect(items[0].quality).toEqual(23);
+        });
+
+        it("Tests if the quality value drops to 0 when sellIn = 0", function () {
+            const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20)]);
+            const items = gildedRose.updateQuality();
+            expect(items[0].quality).toEqual(0);
+        });
     });
 
 });
