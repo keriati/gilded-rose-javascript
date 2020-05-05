f08e594595e36d0a9c365542ccb4bd51f08ac940
Hai Ha Nguyen <haicorp87@gmai.com>
2020-05-05 15:53:52 +0200

    adding new test for the app

--- src/__tests__/gilded_rose_spec.js
+++ src/__tests__/gilded_rose_spec.js
@@ -1,11 +1,45 @@
 import { Shop, Item } from '../gilded_rose';
 
-describe("Gilded Rose", function () {
+describe('Gilded Rose', function () {
+  it('should foo', function () {
+    const gildedRose = new Shop([new Item('foo', 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toEqual('foo');
+  });
 
-    it("should foo", function () {
-        const gildedRose = new Shop([new Item("foo", 0, 0)]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).toEqual("fixme");
-    });
+  it('new Test', function () {
+    const myCustomShop = new Shop([new Item('newItem', 6, 2)]);
+    const items = myCustomShop.updateQuality();
+    expect(items[0].quality).toEqual(1);
+  });
 
+  it('selling Test - lowering sellin', function () {
+    const myCustomShop = new Shop([new Item('newItem2', 4, 2)]);
+    const items = myCustomShop.updateQuality();
+    expect(items[0].sellIn).toEqual(3);
+  });
+
+  it('selling Test 3 - degrading quality', function () {
+    const myCustomShop = new Shop([new Item('newItem3', 0, 2)]);
+    const items = myCustomShop.updateQuality();
+    expect(items[0].quality).toEqual(0);
+  });
+
+  it('selling Test 4 - quality is never negative', function () {
+    const myCustomShop = new Shop([new Item('newItem4', 2, 0)]);
+    const items = myCustomShop.updateQuality();
+    expect(items[0].quality).toEqual(0);
+  });
+
+  it('selling Test 5 - "Aged Brie" actually increases in Quality the older it gets', function () {
+    const myCustomShop = new Shop([new Item('Aged Brie', 2, 2)]);
+    const items = myCustomShop.updateQuality();
+    expect(items[0].quality).toEqual(3);
+  });
+
+  it('selling Test 6 - The Quality of an item is never more than 50', function () {
+    const myCustomShop = new Shop([new Item('Aged Brie', 2, 50)]);
+    const items = myCustomShop.updateQuality();
+    expect(items[0].quality).toEqual(50);
+  });
 });
--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -1,5 +1,7 @@
+// for more cleaner way, I would seperate two classes to two files
+
 export class Item {
-  constructor(name, sellIn, quality){
+  constructor(name, sellIn, quality) {
     this.name = name;
     this.sellIn = sellIn;
     this.quality = quality;
@@ -7,12 +9,18 @@ export class Item {
 }
 
 export class Shop {
-  constructor(items=[]){
+  constructor(items = []) {
     this.items = items;
   }
+
+  // there are a lot of if/else, maybe use switch statement?
+
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+      if (
+        this.items[i].name != 'Aged Brie' &&
+        this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert'
+      ) {
         if (this.items[i].quality > 0) {
           if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
             this.items[i].quality = this.items[i].quality - 1;
@@ -21,7 +29,9 @@ export class Shop {
       } else {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+          if (
+            this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert'
+          ) {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
                 this.items[i].quality = this.items[i].quality + 1;
@@ -40,14 +50,17 @@ export class Shop {
       }
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+          if (
+            this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert'
+          ) {
             if (this.items[i].quality > 0) {
               if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
                 this.items[i].quality = this.items[i].quality - 1;
               }
             }
           } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
+            this.items[i].quality =
+              this.items[i].quality - this.items[i].quality;
           }
         } else {
           if (this.items[i].quality < 50) {
