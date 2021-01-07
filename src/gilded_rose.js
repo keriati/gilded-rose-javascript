e1bf817cc423ba9da420763b7a84dc67c2eb224e
Jan Capiak <jancapiak@gmail.com>
2021-01-07 15:54:13 +0100

    Extracts methods for checking item type

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -23,21 +23,29 @@ export class Shop {
     return this.equalsToItemName(itemName, AGED_BRIE_ITEM);
   }
 
+  isBackstagePassesItem(itemName) {
+    return this.equalsToItemName(itemName, BACKSTAGE_PASSES_ITEM);
+  }
+
+  isSulfurasItem(itemName) {
+    return this.equalsToItemName(itemName, SULFURAS_ITEM);
+  }
+
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
       if (
         !this.isAgedBrieItem(this.items[i].name) &&
-        this.items[i].name != BACKSTAGE_PASSES_ITEM
+        !this.isBackstagePassesItem(this.items[i].name)
       ) {
         if (this.items[i].quality > 0) {
-          if (this.items[i].name != SULFURAS_ITEM) {
+          if (!this.isSulfurasItem(this.items[i].name)) {
             this.items[i].quality = this.items[i].quality - 1;
           }
         }
       } else {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == BACKSTAGE_PASSES_ITEM) {
+          if (this.isBackstagePassesItem(this.items[i].name)) {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
                 this.items[i].quality = this.items[i].quality + 1;
@@ -51,14 +59,14 @@ export class Shop {
           }
         }
       }
-      if (this.items[i].name != SULFURAS_ITEM) {
+      if (!this.isSulfurasItem(this.items[i].name)) {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
       if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != AGED_BRIE_ITEM) {
-          if (this.items[i].name != BACKSTAGE_PASSES_ITEM) {
+        if (!this.isAgedBrieItem(this.items[i].name)) {
+          if (!this.isBackstagePassesItem(this.items[i].name)) {
             if (this.items[i].quality > 0) {
-              if (this.items[i].name != SULFURAS_ITEM) {
+              if (!this.isSulfurasItem(this.items[i].name)) {
                 this.items[i].quality = this.items[i].quality - 1;
               }
             }
