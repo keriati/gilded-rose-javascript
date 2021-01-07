d6409dc36ab450e57327fa8a642f288a61a71996
Jan Capiak <jancapiak@gmail.com>
2021-01-07 16:01:31 +0100

    Created quality and sell in constants

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -10,6 +10,11 @@ const AGED_BRIE_ITEM = "Aged Brie";
 const BACKSTAGE_PASSES_ITEM = "Backstage passes to a TAFKAL80ETC concert";
 const SULFURAS_ITEM = "Sulfuras, Hand of Ragnaros";
 
+const MAX_QUALITY = 50;
+const MIN_QUALITY = 0;
+const SELL_IN_11 = 11;
+const SELL_IN_6 = 6;
+
 export class Shop {
   constructor(items = []) {
     this.items = items;
@@ -37,22 +42,22 @@ export class Shop {
         !this.isAgedBrieItem(this.items[i].name) &&
         !this.isBackstagePassesItem(this.items[i].name)
       ) {
-        if (this.items[i].quality > 0) {
+        if (this.items[i].quality > MIN_QUALITY) {
           if (!this.isSulfurasItem(this.items[i].name)) {
             this.items[i].quality = this.items[i].quality - 1;
           }
         }
       } else {
-        if (this.items[i].quality < 50) {
+        if (this.items[i].quality < MAX_QUALITY) {
           this.items[i].quality = this.items[i].quality + 1;
           if (this.isBackstagePassesItem(this.items[i].name)) {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
+            if (this.items[i].sellIn < SELL_IN_11) {
+              if (this.items[i].quality < MAX_QUALITY) {
                 this.items[i].quality = this.items[i].quality + 1;
               }
             }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
+            if (this.items[i].sellIn < SELL_IN_6) {
+              if (this.items[i].quality < MAX_QUALITY) {
                 this.items[i].quality = this.items[i].quality + 1;
               }
             }
@@ -62,10 +67,10 @@ export class Shop {
       if (!this.isSulfurasItem(this.items[i].name)) {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
-      if (this.items[i].sellIn < 0) {
+      if (this.items[i].sellIn < MIN_QUALITY) {
         if (!this.isAgedBrieItem(this.items[i].name)) {
           if (!this.isBackstagePassesItem(this.items[i].name)) {
-            if (this.items[i].quality > 0) {
+            if (this.items[i].quality > MIN_QUALITY) {
               if (!this.isSulfurasItem(this.items[i].name)) {
                 this.items[i].quality = this.items[i].quality - 1;
               }
@@ -75,7 +80,7 @@ export class Shop {
               this.items[i].quality - this.items[i].quality;
           }
         } else {
-          if (this.items[i].quality < 50) {
+          if (this.items[i].quality < MAX_QUALITY) {
             this.items[i].quality = this.items[i].quality + 1;
           }
         }
