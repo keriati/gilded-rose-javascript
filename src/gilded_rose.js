030b2ee6a08f0bf825a5b36b88efca2b957194fe
Marton Gaspardy <gaspy89@gmail.com>
2021-02-18 16:22:15 +0100

    introduced individual item update functions

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -7,18 +7,13 @@ export class Item {
 }
 
 const MAX_QUALITY = 50;
+const MIN_QUALITY = 0;
 
-const isAgedBrie = (item) => {
-  return item.name === 'Aged Brie';
-}
+const isAgedBrie = ({name}) => name === 'Aged Brie';
 
-const isBackStagePasses = (item) => {
-  return item.name === 'Backstage passes to a TAFKAL80ETC concert';
-}
+const isBackStagePasses = ({name}) => name === 'Backstage passes to a TAFKAL80ETC concert';
 
-const isSulfuras = (item) => {
-  return item.name === 'Sulfuras, Hand of Ragnaros';
-}
+const isSulfuras = ({name}) => name === 'Sulfuras, Hand of Ragnaros';
 
 const increaseItemQuality = (item) => {
   if (item.quality < MAX_QUALITY) {
@@ -27,7 +22,7 @@ const increaseItemQuality = (item) => {
 }
 
 const decreaseItemQuality = (item) => {
-  if (item.quality > 0) {
+  if (item.quality > MIN_QUALITY) {
     item.quality = item.quality - 1;
   }
 }
@@ -36,41 +31,60 @@ const decreaseSellInValue = (item) => {
   item.sellIn = item.sellIn - 1;
 }
 
+const isExpired = ({sellIn}) => sellIn < 0;
+
+
+const updateAgedBrieItem = (item) => {
+  decreaseSellInValue(item);
+  increaseItemQuality(item);
+  if (isExpired(item)) {
+    increaseItemQuality(item);
+  }
+}
+
+const updateBackStageItem = (item) => {
+  increaseItemQuality(item);
+  if (item.sellIn < 11) {
+    increaseItemQuality(item);
+  }
+  if (item.sellIn < 6) {
+    increaseItemQuality(item);
+  }
+  decreaseSellInValue(item);
+  if (isExpired(item)) {
+    item.quality = MIN_QUALITY;
+  }
+}
+
+function updateDefaultItem(item) {
+  decreaseSellInValue(item);
+  decreaseItemQuality(item);
+  if (isExpired(item)) {
+    decreaseItemQuality(item);
+  }
+}
+
 const updateItem = (item) => {
 
   if(isSulfuras(item)) {
     return;
   }
 
-  if (!isAgedBrie(item) && !isBackStagePasses(item)) {
-    decreaseItemQuality(item);
-  } else {
-    increaseItemQuality(item);
-    if (isBackStagePasses(item)) {
-      if (item.sellIn < 11) {
-        increaseItemQuality(item);
-      }
-      if (item.sellIn < 6) {
-        increaseItemQuality(item);
-      }
-    }
+  if(isAgedBrie(item)) {
+    updateAgedBrieItem(item);
+    return;
   }
-  decreaseSellInValue(item);
-  if (item.sellIn < 0) {
-    if (!isAgedBrie(item)) {
-      if (!isBackStagePasses(item)) {
-        decreaseItemQuality(item);
-      } else {
-        item.quality = item.quality - item.quality;
-      }
-    } else {
-      increaseItemQuality(item);
-    }
+
+  if (isBackStagePasses(item)) {
+    updateBackStageItem(item);
+    return;
   }
+
+  updateDefaultItem(item);
 }
 
 export class Shop {
-  constructor(items=[]){
+  constructor(items = []) {
     this.items = items;
   }
   updateQuality() {
