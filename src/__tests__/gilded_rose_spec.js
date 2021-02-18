3b323f2fefbc80b83179ec970ecee5eb98e4c916
Marton Gaspardy <gaspy89@gmail.com>
2021-02-18 14:41:52 +0100

    wrote tests for Default item

--- src/__tests__/gilded_rose_spec.js
+++ src/__tests__/gilded_rose_spec.js
@@ -1,11 +1,51 @@
 import { Shop, Item } from '../gilded_rose';
 
-describe("Gilded Rose", function () {
+describe("Shop.updateQuality()",  () => {
+    it("Never changes the name",  () => {
+        const gildedRose = new Shop([new Item("name", 0, 0)]);
 
-    it("should foo", function () {
-        const gildedRose = new Shop([new Item("foo", 0, 0)]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).toEqual("fixme");
+        gildedRose.updateQuality();
+        const updatedItem = gildedRose.items[0];
+
+        expect(updatedItem.name).toEqual("name");
+    });
+
+    describe("Default item", () => {
+       it("Decreases sellIn value by 1 at the end of the day", () => {
+           const gildedRose = new Shop([new Item("Default item", 5, 5)]);
+
+           gildedRose.updateQuality();
+           const updatedItem = gildedRose.items[0];
+
+           expect(updatedItem.sellIn).toEqual(4);
+       });
+
+        it("Decreases Quality value by 1 at the end of the day if item is not expired", () => {
+            const gildedRose = new Shop([new Item("Default item", 5, 5)]);
+
+            gildedRose.updateQuality();
+            const updatedItem = gildedRose.items[0];
+
+            expect(updatedItem.quality).toEqual(4);
+        });
+
+        it("Decreases Quality value by 2 at the end of the day if item is expired", () => {
+            const gildedRose = new Shop([new Item("Default item", 0, 5)]);
+
+            gildedRose.updateQuality();
+            const updatedItem = gildedRose.items[0];
+
+            expect(updatedItem.quality).toEqual(3);
+        });
+
+        it("does not decrease quality value below 0", () => {
+            const gildedRose = new Shop([new Item("Default item", 0, 0)]);
+
+            gildedRose.updateQuality();
+            const updatedItem = gildedRose.items[0];
+
+            expect(updatedItem.quality).toEqual(0);
+        });
     });
 
 });
