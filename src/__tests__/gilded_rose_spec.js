ad95baeb52d2772fa8f91ce6e1fbd01e191da54a
Vlad Slavin <vladimir.slavin@external.erstegroup.com>
2021-03-26 10:17:16 +0100

    test: GFE-123 Add tests for Backstage Passes

--- src/__tests__/gilded_rose_spec.js
+++ src/__tests__/gilded_rose_spec.js
@@ -2,13 +2,22 @@ import { Shop, Item } from '../gilded_rose';
 
 const SELLIN_ZERO = 0;
 const SELLIN_ONE = 1;
+const SELLIN_FIVE = 5;
+const SELLIN_TEN = 10;
+const SELLIN_ELEVEN = 11;
+
 const QUALITY_MIN = 0;
 const QUALITY_ONE = 1;
 const QUALITY_TWO = 2;
+const QUALITY_48 = 48;
+const QUALITY_49 = 49;
+const QUALITY_MAX = 50;
+
 const DEFAULT_ITEM = "foo";
 const AGED_BRIE = "Aged Brie";
 const SULFURAS = "Sulfuras, Hand of Ragnaros";
-const QUALITY_MAX = 50;
+const BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert";
+
 
 function getShopWithItem(name, sellin, quality) {
     const gildedRose = new Shop([new Item(name, sellin, quality)]);
@@ -17,13 +26,21 @@ function getShopWithItem(name, sellin, quality) {
 
 describe("Gilded Rose #updateQuality()", function () {
 
-    describe('default item', function () {
-        it("decreases sellIn by 1", function () {
+    describe('Default Item', function () {
+        it("should decrease sellIn by 1", function () {
             const gildedRose = getShopWithItem(DEFAULT_ITEM, SELLIN_ONE, QUALITY_MIN);
 
             const items = gildedRose.updateQuality();
 
             expect(items[0].sellIn).toEqual(0);
+        });
+
+        it("should not decrease quality by 1 when expired and quality is zero", function () {
+            const gildedRose = getShopWithItem(DEFAULT_ITEM, SELLIN_ZERO, QUALITY_MIN);
+
+            const items = gildedRose.updateQuality();
+
+            expect(items[0].quality).toEqual(QUALITY_MIN);
         });
 
         it('should decrease quality by 1', function () {
@@ -76,6 +93,14 @@ describe("Gilded Rose #updateQuality()", function () {
             expect(items[0].quality).toEqual(QUALITY_MAX);
         });
 
+        it('should never increase the quality over 50 when expired', function () {
+            const gildedRose = getShopWithItem(AGED_BRIE, SELLIN_ZERO, QUALITY_MAX);
+
+            const items = gildedRose.updateQuality();
+
+            expect(items[0].quality).toEqual(QUALITY_MAX);
+        });
+
         it('should increase quality 2x when expired', function () {
             const gildedRose = getShopWithItem(AGED_BRIE, SELLIN_ZERO, QUALITY_TWO);
 
@@ -101,7 +126,72 @@ describe("Gilded Rose #updateQuality()", function () {
 
             expect(items[0].quality).toEqual(QUALITY_ONE);
         });
+
+        it('should not change quality when expired', function () {
+            const gildedRose = getShopWithItem(SULFURAS, -1, QUALITY_ONE);
+
+            const items = gildedRose.updateQuality();
+
+            expect(items[0].quality).toEqual(QUALITY_ONE);
+        });
     });
 
+    describe('Backstage Pass', function () {
+        it("should decrease sellIn by 1", function () {
+            const gildedRose = getShopWithItem(BACKSTAGE_PASS, SELLIN_ELEVEN, QUALITY_MAX);
 
+            const items = gildedRose.updateQuality();
+
+            expect(items[0].sellIn).toEqual(SELLIN_TEN);
+        });
+
+        it("should expire after the sellIn", function () {
+            const gildedRose = getShopWithItem(BACKSTAGE_PASS, SELLIN_ZERO, QUALITY_MAX);
+
+            const items = gildedRose.updateQuality();
+
+            expect(items[0].quality).toEqual(QUALITY_MIN);
+        });
+
+        it('should increase quality by 1 when there are more than 10 days left', function () {
+            const gildedRose = getShopWithItem(BACKSTAGE_PASS, SELLIN_ELEVEN, QUALITY_MIN);
+
+            const items = gildedRose.updateQuality();
+
+            expect(items[0].quality).toEqual(QUALITY_ONE);
+        });
+
+        it('should not increase quality when there are more than 10 days left and quality is already max', function () {
+            const gildedRose = getShopWithItem(BACKSTAGE_PASS, SELLIN_TEN, QUALITY_49);
+
+            const items = gildedRose.updateQuality();
+
+            expect(items[0].quality).toEqual(QUALITY_MAX);
+        });
+
+
+        it('should increase in quality by 2 when there are 10 days or less left', function () {
+            const gildedRose = getShopWithItem(BACKSTAGE_PASS, SELLIN_TEN, QUALITY_MIN);
+
+            const items = gildedRose.updateQuality();
+
+            expect(items[0].quality).toEqual(2);
+        });
+
+        it('should increase in quality by 3 when there are 5 days or less left', function () {
+            const gildedRose = getShopWithItem(BACKSTAGE_PASS, SELLIN_FIVE, QUALITY_MIN);
+
+            const items = gildedRose.updateQuality();
+
+            expect(items[0].quality).toEqual(3);
+        });
+
+        it('should not increase quality when there are 5 days or less left and quality is already max', function () {
+            const gildedRose = getShopWithItem(BACKSTAGE_PASS, SELLIN_FIVE, QUALITY_48);
+
+            const items = gildedRose.updateQuality();
+
+            expect(items[0].quality).toEqual(QUALITY_MAX);
+        });
+    });
 });
