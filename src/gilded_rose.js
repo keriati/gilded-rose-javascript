dbbcf29d7559bf76d53a77f1101fd1327bcf49ca
navneetsharma <sharma.navneet1988@gmail.com>
2021-07-29 19:37:58 +0530

    Inital refactoring - added constants for magic strings, handle Sulfuras & Conjured item scenario

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -1,3 +1,8 @@
+export const SULFURAS = 'Sulfuras, Hand of Ragnaros';
+export const AGED_BRIE = 'Aged Brie';
+export const BACKSTAGE_PASSES = 'Backstage passes to a TAFKAL80ETC concert';
+export const CONJURED = 'Conjured';
+
 export class Item {
   constructor(name, sellIn, quality){
     this.name = name;
@@ -6,22 +11,42 @@ export class Item {
   }
 }
 
+
 export class Shop {
   constructor(items=[]){
     this.items = items;
   }
   updateQuality() {
+
     for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+      const item = this.items[i];
+      const { name } = item;
+      // if item is SULFURAS, don't degrade the quality and sellIn value
+      if (name === SULFURAS) {
+        return this.items;
+      }
+
+      // if item is CONJURED, degrade the quality twice as compare to normal items
+      if (name === CONJURED ) {
+        this.items[i] = {
+          ...this.items[i],
+          quality: item.quality - 2,
+          sellIn: item.sellIn - 1
+
+        }
+        return this.items;
+      }
+
+      if (this.items[i].name !== AGED_BRIE && this.items[i].name !== BACKSTAGE_PASSES) {
         if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+          if (this.items[i].name !== SULFURAS) {
             this.items[i].quality = this.items[i].quality - 1;
           }
         }
       } else {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+          if (this.items[i].name === BACKSTAGE_PASSES) {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
                 this.items[i].quality = this.items[i].quality + 1;
@@ -35,14 +60,14 @@ export class Shop {
           }
         }
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+      if (this.items[i].name !== SULFURAS) {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
       if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+        if (this.items[i].name !== AGED_BRIE) {
+          if (this.items[i].name !== BACKSTAGE_PASSES) {
             if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+              if (this.items[i].name !== SULFURAS) {
                 this.items[i].quality = this.items[i].quality - 1;
               }
             }
