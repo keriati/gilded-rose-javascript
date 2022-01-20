9c817dffcbabe3b8168dd48df97f4f45bda1028a
Filip Messa <messa.filip@gmail.com>
2022-01-20 14:56:54 +0100

    refactor

--- src/__tests__/gilded_rose_spec.js
+++ src/__tests__/gilded_rose_spec.js
@@ -1,11 +1,79 @@
-import { Shop, Item } from '../gilded_rose';
+import { Shop, Item } from "../gilded_rose";
+import { BackstagePasses } from "../gilded_rose";
 
 describe("Gilded Rose", function () {
+  it("Once the sell by date has passed, Quality degrades twice as fast", () => {
+    const gildedRose = new Shop([new Item("foo", 0, 50)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(48);
+  });
 
-    it("should foo", function () {
-        const gildedRose = new Shop([new Item("foo", 0, 0)]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).toEqual("fixme");
-    });
+  it("Once the sell by date has did't passe, Quality degrades down by 1", () => {
+    const gildedRose = new Shop([new Item("foo", 1, 50)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(49);
+  });
 
+  it("The Quality of an item is never negative", () => {
+    const gildedRose = new Shop([new Item("foo", 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(0);
+  });
+
+  it("Aged Brie' actually increases in Quality the older it gets", () => {
+    const gildedRose = new Shop([new Item("Aged Brie", 1, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(1);
+  });
+  it("Once the sell by date has passed, Quality of the Aged Brie sould increse twice as fast", () => {
+    const gildedRose = new Shop([new Item("Aged Brie", 0, 1)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(3);
+  });
+
+  it("The Quality of an item is never more than 50", () => {
+    const gildedRose = new Shop([new Item("Aged Brie", 0, 50)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(50);
+  });
+
+  it("'Sulfuras, Hand of Ragnaros', being a legendary item, never has to be decreases in Quality", () => {
+    const gildedRose = new Shop([
+      new Item("Sulfuras, Hand of Ragnaros", 1, 50),
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(50);
+  });
+
+  it("'Sulfuras, Hand of Ragnaros', being a legendary item, never ever has to be sold", () => {
+    const gildedRose = new Shop([
+      new Item("Sulfuras, Hand of Ragnaros", 10, 50),
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toEqual(10);
+  });
+
+  it("'Backstage passes to a TAFKAL80ETC concert' actually increases in Quality the older it gets", () => {
+    const gildedRose = new Shop([new Item(BackstagePasses, 12, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(1);
+  });
+
+  it("'Backstage passes to a TAFKAL80ETC concert' quality increases by 3 when there are 5 days or less", () => {
+    const gildedRose = new Shop([new Item(BackstagePasses, 5, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(3);
+  });
+
+  it("'Backstage passes to a TAFKAL80ETC concert' quality increases by 2 when there are 10 days or less", () => {
+    const gildedRose = new Shop([new Item(BackstagePasses, 10, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(2);
+  });
+
+  it("'Backstage passes to a TAFKAL80ETC concert' quality drops to 0 after the concert", () => {
+    const gildedRose = new Shop([new Item(BackstagePasses, 0, 50)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(0);
+  });
 });
