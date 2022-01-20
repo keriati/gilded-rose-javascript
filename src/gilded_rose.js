0d36973cdfff922a63e169107463dbea9f9ddecb
Filip Messa <messa.filip@gmail.com>
2022-01-20 15:12:21 +0100

    refactor 4

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -14,55 +14,51 @@ export class Shop {
     this.items = items;
   }
   updateQuality() {
-    for (var i = 0; i < this.items.length; i++) {
-      if (
-        this.items[i].name != AgedBrie &&
-        this.items[i].name != BackstagePasses
-      ) {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != Sulfuras) {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
+    this.items.forEach((item) => {
+      if (item.name === Sulfuras) {
+        return this.items;
+      }
+
+      if (item.name != AgedBrie && item.name != BackstagePasses) {
+        if (item.quality > 0) {
+          item.quality = item.quality - 1;
         }
       } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == BackstagePasses) {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
+        if (item.quality < 50) {
+          item.quality = item.quality + 1;
+          if (item.name == BackstagePasses) {
+            if (item.sellIn < 11) {
+              if (item.quality < 50) {
+                item.quality = item.quality + 1;
               }
             }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
+            if (item.sellIn < 6) {
+              if (item.quality < 50) {
+                item.quality = item.quality + 1;
               }
             }
           }
         }
       }
-      if (this.items[i].name != Sulfuras) {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != AgedBrie) {
-          if (this.items[i].name != BackstagePasses) {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != Sulfuras) {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
+
+      item.sellIn = item.sellIn - 1;
+
+      if (item.sellIn < 0) {
+        if (item.name != AgedBrie) {
+          if (item.name != BackstagePasses) {
+            if (item.quality > 0) {
+              item.quality = item.quality - 1;
             }
           } else {
-            this.items[i].quality =
-              this.items[i].quality - this.items[i].quality;
+            item.quality = item.quality - item.quality;
           }
         } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
+          if (item.quality < 50) {
+            item.quality = item.quality + 1;
           }
         }
       }
-    }
+    });
 
     return this.items;
   }
